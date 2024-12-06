b9709c35218cd2d9bf94cfebef995694dac80996
Antonio-Savio <antonio.savio28@gmail.com>
2024-12-06 01:34:52 -0300

    perf: add suspense UI and enhance metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
+import { Suspense } from 'react'
 import type { Metadata } from "next";
 import "./globals.scss";
 import { Header } from "@/components/header";
+import Loading from '@/app/loading'
 
 export const metadata: Metadata = {
   title: "Gatinhos Cajueiros",
   description: "Somos um projeto que viabiliza a adoção, alimentação e castração de gatos no Parque dos Cajueiros em Aracaju-SE",
+  keywords: ['gatinhos cajueiros', 'adoção aracaju', 'adoção de gatos', 'gatos', 'adotar gatos Aracaju', 'castração de gatos', 'adoção sergipe'],
+  openGraph: {
+    title: "Gatinhos Cajueiros",
+    images: [`${process.env.NEXT_PUBLIC_URL}/logo_og.jpg`]
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    }
+  }
 };
 
 export default function RootLayout({
@@ -13,10 +30,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>
         <Header/>
-        {children}
+        <Suspense fallback={<Loading/>}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
