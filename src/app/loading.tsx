a8f46eaa3384521362c8693ded9673ad4a4fd256
Antonio-Savio <antonio.savio28@gmail.com>
2024-12-06 01:40:54 -0300

    chore: basic style changing

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import { TbLoaderQuarter } from "react-icons/tb";
 export default function Loading(){
     return(
         <main className={loadStyles.wrapper}>
-            <p>Carregando <TbLoaderQuarter color='#16b0b0'/></p>
+            <p>Carregando <TbLoaderQuarter/></p>
         </main>
     )
 }
